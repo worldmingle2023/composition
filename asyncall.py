9b0c20cf5881466d1817a2c0567fb03bced4bad1
ws2694 <ws2694@columbia.edu>
2023-12-22 13:48:02 -0500

    added user microservice

--- asyncall.py
+++ asyncall.py
@@ -4,13 +4,14 @@ import aiohttp
 microservices = [
     "http://ec2-3-22-186-8.us-east-2.compute.amazonaws.com:5000/",
     "https://1qajdtdqj3.execute-api.us-west-2.amazonaws.com",
-
+    "https://cloudcomputing-worldmingle.ue.r.appspot.com/"
 ]
 
 async def async_call_microservice(session, url):
     async with session.get(url) as response:
         response_text = await response.text()
         print(f"Received response from {url}: {response_text}")
+        print("-----------------------------------------------")
 
 async def asynchronous_calls():
     async with aiohttp.ClientSession() as session:
@@ -18,5 +19,13 @@ async def asynchronous_calls():
         await asyncio.gather(*tasks)
 
 # Repeat 10 times
-for _ in range(10):
+for _ in range(5):
+    asyncio.run(asynchronous_calls())
+
+microservices = [
+    "https://cloudcomputing-worldmingle.ue.r.appspot.com/",
+    "https://1qajdtdqj3.execute-api.us-west-2.amazonaws.com",
+     "http://ec2-3-22-186-8.us-east-2.compute.amazonaws.com:5000/",
+]
+for _ in range(5):
     asyncio.run(asynchronous_calls())
--- synccall.py
+++ synccall.py
@@ -2,13 +2,16 @@ import requests
 
 microservices = [
     "http://ec2-3-22-186-8.us-east-2.compute.amazonaws.com:5000/",
-    "https://1qajdtdqj3.execute-api.us-west-2.amazonaws.com"
+    "https://1qajdtdqj3.execute-api.us-west-2.amazonaws.com",
+    "https://cloudcomputing-worldmingle.ue.r.appspot.com/"
+
 
 ]
 
 def call_microservice(url):
     response = requests.get(url)
     print(f"Received response from {url}: {response.text}")
+    print("-----------------------------------------------")
 
 def synchronous_calls():
     for url in microservices:
